c6a1b93f302f52c9aa9bcdb77aec938536d032b2
Mofolabi Osotimehin <mofolabi.osotimehin@student.moringaschool.com>
2023-01-25 14:21:12 +0100

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(superbowls){
+    const win = superbowls.find(game => game.result === 'W');
+    if(win){
+        return win.year;
+    }else{
+        return undefined;
+    }
+}
